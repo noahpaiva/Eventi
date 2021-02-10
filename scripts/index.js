c2e248a92570c306350afd088cee2f75dd958d22
Holly Bernich <55116533+hollyyberr@users.noreply.github.com>
2021-02-10 00:51:38 -0500

    adding service worker to the application in js

--- scripts/index.js
+++ scripts/index.js
@@ -6,4 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
     var items = document.querySelectorAll('.collapsible');
     M.Collapsible.init(items);
     
+    if("serviceWorker" in navigator){
+        navigator.serviceWorker.register(sw.js).then(registration => {
+            console.log("SW Registered!");
+            console.log(registration);
+    }.catch(error){
+            console.log("SW Registration failed!");
+            console.log(error);
+        }
+    
 })
