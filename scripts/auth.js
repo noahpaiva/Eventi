f4c8ff8435c0b5c9b7954adeb28cdee9bc8f159a
noahpaiva <78582698+noahpaiva@users.noreply.github.com>
2021-02-09 21:15:27 -0500

    Total Overhaul
    Better looking UI.
    Reworked completely.
    User sign up functionality working properly.

---
+++ scripts/auth.js
@@ -0,0 +1,17 @@
+// signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    //get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // sign up user, asynchronous task, takes time to complete, requires promise to function correctly
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    });
+})
