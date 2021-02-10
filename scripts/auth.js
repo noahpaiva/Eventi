52cc61eb3b51f1e30b4337ee40e109e67391ec0c
noahpaiva <78582698+noahpaiva@users.noreply.github.com>
2021-02-09 22:49:30 -0500

    Continued development
    Added checks for user login status.
    Fixed logout button error.

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,18 @@
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+
+    if (user) {
+        console.log('user logged in: ', user);
+    }
+    else {
+        console.log('user logged out');
+    }
+
+});
+
+
+
+
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
@@ -9,9 +24,32 @@ signupForm.addEventListener('submit', (e) => {
 
     // sign up user, asynchronous task, takes time to complete, requires promise to function correctly
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset();
     });
 })
+
+
+
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+});
+
+
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+});
