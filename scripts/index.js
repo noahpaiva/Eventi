b884e24cce9d9f2cac02077de40702a3c4d9fb3a
noahpaiva <78582698+noahpaiva@users.noreply.github.com>
2021-02-23 17:01:10 -0500

    Carousel Fixed

--- scripts/index.js
+++ scripts/index.js
@@ -88,10 +88,7 @@ const setupEvents = (data) => {
                 var loc = event.Location.replace(/\s/g, '+');
                 loc.replace(/,/g, '%2C');
                 let li = `
-<<<<<<< Updated upstream
-=======
                     <li>
->>>>>>> Stashed changes
                         <div class="collapsible-header orange lighten-4">${event.Title}</div>
                         <div class="collapsible-body light orange lighten-5">
                             ${event.Desc} <br />
@@ -108,43 +105,42 @@ const setupEvents = (data) => {
         eventList.innerHTML = html;
     }
     else {
-<<<<<<< Updated upstream
-        <li><div id="myCarousel" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                      <li data-target="#myCarousel" data-slide-to="1"></li>
-                      <li data-target="#myCarousel" data-slide-to="2"></li>
-                    </ol>
-                  
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner">
-                      <div class="item active">
-                        <img src="event.jpg" alt="Plan extravagant events with Eventi!">
-                      </div>
-                  
-                      <div class="item">
-                        <img src="event2.jpg" alt="Your next event has never been this easy!">
-                      </div>
-                  
-                      <div class="item">
-                        <img src="event3.jpg" alt="Enjoy an adventure or a relaxing day, it's all up to you!">
-                      </div>
-                    </div>
-                  
-                    <!-- Left and right controls -->
-                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                      <span class="glyphicon glyphicon-chevron-left"></span>
-                      <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                      <span class="glyphicon glyphicon-chevron-right"></span>
-                      <span class="sr-only">Next</span>
-                    </a>
-                  </div>
-=======
->>>>>>> Stashed changes
-        eventList.innerHTML = '<h5 class="center-align">Please login to view your events!</h5>';
+        let li = `
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+            
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                <img src="event.jpg" alt="Plan extravagant events with Eventi!">
+                </div>
+            
+                <div class="item">
+                <img src="event2.jpg" alt="Your next event has never been this easy!">
+                </div>
+            
+                <div class="item">
+                <img src="event3.jpg" alt="Enjoy an adventure or a relaxing day, it's all up to you!">
+                </div>
+            </div>
+            
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+        `;
+        eventList.innerHTML = li;
         
     }   
 }
