bb10b6de6591dae27962eaa5862a772073d1041f
noahpaiva <78582698+noahpaiva@users.noreply.github.com>
2021-02-25 17:55:38 -0500

    Adjusted sizing

--- scripts/index.js
+++ scripts/index.js
@@ -106,39 +106,7 @@ const setupEvents = (data) => {
     }
     else {
         let li = `
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-            
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                <div class="item active">
-                <img src="img/event.jpg" alt="Plan extravagant events with Eventi!">
-                </div>
-            
-                <div class="item">
-                <img src="img/event2.jpg" alt="Your next event has never been this easy!">
-                </div>
-            
-                <div class="item">
-                <img src="img/event3.jpg" alt="Enjoy an adventure or a relaxing day, it's all up to you!">
-                </div>
-            </div>
-            
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
+        <h5>Please log in to view your events.</h5>
         `;
         eventList.innerHTML = li;
         
