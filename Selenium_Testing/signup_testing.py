c96d513bb395984d8a967c0ab314a01f04b9d9eb
noahpaiva <78582698+noahpaiva@users.noreply.github.com>
2021-04-16 08:34:48 -0400

    Delete Selenium_Testing directory

--- Selenium_Testing/signup_testing.py
+++
@@ -1,8 +0,0 @@
-# signup_testing.py - Used to test user sign up functionality
-
-from selenium import webdriver
-import chromedriver_binary
-
-
-driver = webdriver.Chrome()
-driver.get('http://zorak2.monmouth.edu/~s1194309/Eventi/index.html')
