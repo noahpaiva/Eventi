02f13d214a1f83d77a3eb2f857d20c741720fdba
noahpaiva <78582698+noahpaiva@users.noreply.github.com>
2021-04-15 22:39:31 -0400

    Started White-box testing with python

---
+++ Selenium_Testing/signup_testing.py
@@ -0,0 +1,8 @@
+# signup_testing.py - Used to test user sign up functionality
+
+from selenium import webdriver
+import chromedriver_binary
+
+
+driver = webdriver.Chrome()
+driver.get('http://zorak2.monmouth.edu/~s1194309/Eventi/index.html')
